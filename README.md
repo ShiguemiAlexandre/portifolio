27324280499e983cc79297e58b2c860d463a41c8
ShiguemiAlexandre <53339523+ShiguemiAlexandre@users.noreply.github.com>
2023-02-05 21:45:40 -0300

    atualização

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portifolio
+/*Usar o :target para ir para algum seletor sendo ele h1 ou h2 https://www.w3schools.com/cssref/tryit.php?filename=trycss3_target*/
---
+++ js/function.js
@@ -0,0 +1,6 @@
+var clique = document.getElementById("pergaminho");
+
+clique.onclick = function (e) {
+    e.preventDefault();
+    clique.style.marginTop = "0";
+};
