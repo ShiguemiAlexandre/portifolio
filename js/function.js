cb89436b3bd01dd8fb07d28fabd4e58929af3441
ShiguemiAlexandre <53339523+ShiguemiAlexandre@users.noreply.github.com>
2023-03-04 11:10:45 -0300

    att

--- js/function.js
+++ js/function.js
@@ -1,3 +1,29 @@
+function subir_pergaminho() {
+    let close = document.querySelector("#pergaminho");
+    let papel = document.querySelector(".container_main");
+    var paragrafo = document.querySelector(".click_pergaminho");
+    if (close.classList.contains('open')) {
+        close.classList.remove('open');
+        papel.classList.remove('container_papel');
+        paragrafo.classList.remove('abrir')
+        // paragrafo.style.display = "grid";
+    } else {
+        close.classList.add('open');
+        papel.classList.add('container_papel');
+        paragrafo.classList.add('abrir')
+        // paragrafo.style.display = "none";
+    }
+}
+
+//Outra forma de fazer uma troca de posição
+
+// var selecionarpergaminho = document.getElementById("pergaminho");
+
+// selecionarpergaminho.onclick =  _ => {
+//   selecionarpergaminho.style.position = (selecionarpergaminho.style.position === "relative") ? "fixed" : "relative";
+//   selecionarpergaminho.style.top = (selecionarpergaminho.style.position === "fixed") ? "0" : "30vh";
+// };
+
 // var selecionarpergaminho = document.getElementById("pergaminho");
 
 // function open() {
@@ -10,33 +36,3 @@
 //     e.preventDefault();
 //     selecionarpergaminho.classList.toggle('toggle');
 // };let
-function subir_pergaminho() {
-    // document.getElementById("pergaminho").style.transition = "all 3s";
-    // document.getElementById("pergaminho").style.marginTop = "0";
-    let close = document.querySelector("#pergaminho");
-    let papel = document.querySelector(".container_main");
-    var paragrafo = document.getElementById("click_pergaminho");
-    if (close.classList.contains('open')) {
-        close.classList.remove('open');
-        papel.classList.remove('container_papel');
-        paragrafo.style.display = "grid";
-    } else {
-        close.classList.add('open');
-        papel.classList.add('container_papel');
-        paragrafo.style.display = "none";
-    }
-    // paragrafo.style.color = "red";
-    // paragrafo.style.backgroundColor = "yellow";
-    // paragrafo.style.display = "none";
-}
-
-
-
-//Outra forma de fazer uma troca de posição
-
-// var selecionarpergaminho = document.getElementById("pergaminho");
-
-// selecionarpergaminho.onclick =  _ => {
-//   selecionarpergaminho.style.position = (selecionarpergaminho.style.position === "relative") ? "fixed" : "relative";
-//   selecionarpergaminho.style.top = (selecionarpergaminho.style.position === "fixed") ? "0" : "30vh";
-// };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
-var clique = document.getElementById("btn-menu");
-var menuLateral = document.getElementById("menu-lateral");
+const imageViewer = document.querySelector('.image-viewer');
+const previousButton = document.querySelector('.previous');
+const nextButton = document.querySelector('.next');
 
-clique.onclick = function (e) {
-    e.preventDefault();
-    menuLateral.style.marginLeft = "0";
-};
+let currentPosition = 0;
+
+previousButton.addEventListener('click', () => {
+  currentPosition = Math.max(currentPosition - imageViewer.offsetWidth, 0);
+  imageViewer.scrollTo({ left: currentPosition, behavior: 'smooth' });
+});
+
+nextButton.addEventListener('click', () => {
+  currentPosition = Math.min(currentPosition + imageViewer.offsetWidth, imageViewer.scrollWidth - imageViewer.offsetWidth);
+  imageViewer.scrollTo({ left: currentPosition, behavior: 'smooth' });
+});
