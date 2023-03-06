8371f38b9dd087605d951d2d3edbd859b4c13293
ShiguemiAlexandre <53339523+ShiguemiAlexandre@users.noreply.github.com>
2023-03-06 12:39:40 -0300

    ajustes

--- js/function.js
+++ js/function.js
@@ -2,16 +2,15 @@ function subir_pergaminho() {
     let close = document.querySelector("#pergaminho");
     let papel = document.querySelector(".container_main");
     var paragrafo = document.querySelector(".click_pergaminho");
+    
     if (close.classList.contains('open')) {
         close.classList.remove('open');
         papel.classList.remove('container_papel');
-        paragrafo.classList.remove('abrir')
-        // paragrafo.style.display = "grid";
+        paragrafo.classList.remove('abrir');
     } else {
         close.classList.add('open');
         papel.classList.add('container_papel');
-        paragrafo.classList.add('abrir')
-        // paragrafo.style.display = "none";
+        paragrafo.classList.add('abrir');
     }
 }
 
